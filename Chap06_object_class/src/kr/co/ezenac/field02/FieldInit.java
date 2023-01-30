db93f8ccb7b90357002a5f516b197826cdb726a9
levitss <milkis600@gmail.com>
2023-01-30 09:19:36 +0900

    230130

---
+++ Chap06_object_class/src/kr/co/ezenac/field02/FieldInit.java
@@ -0,0 +1,29 @@
+package kr.co.ezenac.field02;
+
+public class FieldInit {
+
+	//기본형 변수
+	byte byteField;				//1byte
+	short shortField;			//2byte
+	int intField;				//4byte
+	long longField;				//8byte
+	
+	boolean booleanField;		//1byte
+	char charField;				//2byte
+	
+	float floatField;			//4
+	double doubleField;			//8
+	
+	//참조형 변수
+	int[] arrField;				//4
+	String strField;			//4
+	
+	public static void main(String[] args) {
+		
+	}
+}
+
+
+
+
+
